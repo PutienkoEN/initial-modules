d77b13b939e83a2b88e266fd0fc7ff243063fc14
Evgenii_Putienko <evgenii_putienko@epam.com>
2019-09-09 13:49:59 +0300

    Change names

--- com.fun.core/src/com/fun/core/dao/UserDao.java
+++ com.fun.core/src/com/fun/core/dao/UserDao.java
@@ -7,7 +7,7 @@ import com.fun.core.models.UserModel;
 public class UserDao
 {
     private static final Map<Long, UserModel> USERS = Map.of(
-            1L, UserModel.from(1L, "Max", "Payne"),
+            1L, UserModel.from(1L, "Mickey", "Mouse"),
             2L, UserModel.from(2L, "Mario", "Mario"),
             3L, UserModel.from(3L, "Luigi", "Mario"));
 
